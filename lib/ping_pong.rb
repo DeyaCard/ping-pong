283c5f699064fadaf9319fc75539324c4087aa44
Deya Card <thedeyacard@gmail.com>
2022-04-12 20:01:56 -0700

    pass second test
    Co-authored-by: Justice Douglas <justice.douglas@hey.com>
    Co-authored-by: Deya Card <thedeyacard@gmail.com>

--- lib/ping_pong.rb
+++ lib/ping_pong.rb
@@ -1,11 +1,26 @@
 require('pry')
 
 def ping_pong(number)
-  return (1..number).to_a
-binding.pry  
+  original_array = (1..number).to_a
+  new_array = []
+  original_array.each do |index|
+    if index.%(3).eql?(0)
+      new_array.push('ping')
+    else 
+      new_array.push(index)  
+    end
+end
+new_array
 end
 
+# Test 1 Notes
 # (1..2).to_a   ------> [1,2]
 # number.to_a ------- [a,b,c,d,...]
 # (2).to_a -----> (1..2).to_a
 # return (1..number).to_a
+
+# Test 2 Notes
+# Pseudocode: if number is divisible by 3 replace with ping
+# Rough Draft: if (number % 3)
+#                 number = 'ping'
+#              end
--- spec/ping_pong_spec.rb
+++ spec/ping_pong_spec.rb
@@ -6,3 +6,10 @@ describe('#ping_pong') do
     expect(ping_pong(2)).to(eq([1,2]))
   end
 end
+
+describe('#ping_pong') do
+  it("returns all numbers divisble by 3 are replaced with 'ping'") do
+    expect(ping_pong(6)).to(eq([1,2,'ping',4,5,'ping']))
+  end
+end   
+
